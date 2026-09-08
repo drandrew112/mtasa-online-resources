8af6dc59981b93c9a1bb6bf673258b6d574e9201
drandrew112 <hevesy.andras@gmail.com>
2026-09-08 07:14:16 +0200

    removed localhost DEV gamemode

--- [core]/v_main/server.lua
+++ [core]/v_main/server.lua
@@ -3,15 +3,6 @@ local version = "0.4.0 Beta"
 function setCustomGamemodeName()
     local serverIP = getServerConfigSetting("serverip")
     local namePrefix = "MTA Online"
-
-    -- Ellenőrizzük, hogy localhost-e (auto, 127.0.0.1 vagy üres)
-    if serverIP == "auto" or serverIP == "127.0.0.1" or serverIP == "" then
-        namePrefix = "MTA Online DEV"
-        outputServerLog("FreeV Gamemode started [DEVELOPMENT MODE]")
-    else
-        outputServerLog("FreeV Gamemode started [PRODUCTION MODE]")
-    end
-
     setGameType(namePrefix .. " v" .. version)
 end
 addEventHandler("onResourceStart", resourceRoot, setCustomGamemodeName)
